c4b52d8beb4990ca73832beee31c8a4887d61010
Raiany Santana <142261731+raianys2@users.noreply.github.com>
2023-09-20 22:14:08 -0300

    versão 1 Hora de codar
    Este projeto foi desenvolvido com base em uma atividade do Instituto Proa, utilizando HTML, CSS e JavaScript

---
+++ horadecodar.js
@@ -0,0 +1,70 @@
+// Atividade 1 
+function ExecAtividade1 () {
+
+var nome_do_carro = "Fusca";
+
+alert ("O nome do carro é: " + nome_do_carro); 
+
+// Atividade 2 
+
+var nome = prompt ("Qual o seu nome?");
+
+alert ("Olá, " + nome + ".");
+
+//Atividade 3
+var idade = prompt ("E qual é a sua idade?");
+
+alert ("Olá, " + nome +"! \nVocê tem " + idade + " anos.");
+
+// Atividade 4 
+
+/*var baseRetangulo = parseInt(prompt ("Qual o valor da base do retângulo?"));
+var alturaDoRetangulo = parseInt (prompt("Qual o valor da altura do retangulo?"));
+var resultado = baseRetangulo * alturaDoRetangulo;
+
+alert ("O resultado do retangulo é: " + resultado);
+
+var lado1quadrado = parseInt(prompt ("Qual o valor de um dos lados do quadrado?"));
+var resultado2 = lado1quadrado * lado1quadrado;
+
+alert ("O resultado do quadrado é: " + resultado2);*/
+
+/*
+var lado1losango = parseInt(prompt ("Qual o valor da diagonal maior do losango?"));
+var lado2losango = parseInt(prompt ("Qual o valor da diagonal menor do losango?"));
+var resultado3 = (lado1losango * lado2losango)/2;
+alert ("O resultado do losango é: " + resultado3); */
+
+
+/*
+var basemaiortarpezio = parseInt(prompt ("Qual o valor da base maior do trapézio?"));
+var basemenortrapezio = parseInt(prompt ("Qual o valor da base menor do trapézio?"));
+var alturatrapezio = parseInt(prompt ("Qual o valor da altura do trapézio?"));
+var resultado4 = ((basemaiortarpezio + basemenortrapezio) * alturatrapezio) /2;
+
+alert ("O resultado do trapézio é: " + resultado4); */
+
+
+/*
+var baseparalelo = parseInt(prompt ("Qual o valor da base do paralelogramo?"));
+var alturaparalelo = parseInt(prompt ("Qual o valor da altura do paralelogramo?"));
+
+var resultado5 = baseparalelo * alturaparalelo;
+
+alert ("O resultado do paralelogramo é: " + resultado5); */
+
+/*
+var basetriagulo = parseInt(prompt ("Qual o valor da base do triângulo?"));
+var alturatriângulo = parseInt(prompt ("Qual o valor da altura do triângulo?"));
+
+var resultado6 = (basetriagulo * alturatriângulo) /2;
+
+alert ("O resultado do triangulo é: " + resultado6); */
+
+var raiocirculo = parseInt(prompt ("Qual o valor do raio do círculo?"));
+
+var resultado7 = Math.PI * Math.pow(raiocirculo, 2);
+
+alert ("O resultado do círculo é: " + resultado7.toFixed(2));
+
+
